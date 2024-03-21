f1de4cd55b65062798b760ed49262df1a1b8b298
MD. Juwel Rana <rk370613@gmail.com>
2024-03-21 14:02:08 +0600

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
 
 // navbar function
 
-const toggle = document.getElementById('three-dought');
-const menu = document.getElementById('nav-list');
+// const toggle = document.getElementById('three-dought');
+// const menu = document.getElementById('nav-list');
 
-toggle.addEventListener('click', function () {
-  if (menu.style.display == 'block') {
-    menu.style.display = 'none'
-  }
-  else {
-    menu.style.display = 'block'
-  }
-})
+// toggle.addEventListener('click', function () {
+//   if (menu.style.display == 'block') {
+//     menu.style.display = 'none'
+//   }
+//   else {
+//     menu.style.display = 'block'
+//   }
+// })
 
 // whtasapp btn function 
 
---
+++ src/js/menu.js
@@ -0,0 +1,29 @@
+
+// const toggle = document.getElementById('toggle');
+// const menu = document.getElementById('mobile-menu');
+// const three = document.getElementById('threed');
+// const xx = document.getElementById('crose');
+
+// toggle.addEventListener('click', function () {
+//   if (menu.style.display == 'block') {
+//     menu.style.display = 'none'
+
+//   }
+//   else {
+//     menu.style.display = 'block'
+//   }
+// })
+
+
+
+const toggle = document.getElementById('three-dought');
+const menu = document.getElementById('nav-list');
+
+toggle.addEventListener('click', function () {
+  if (menu.style.display == 'block') {
+    menu.style.display = 'none'
+  }
+  else {
+    menu.style.display = 'block'
+  }
+})
